7cf0c6541c2fe93e01f83c6c297359627a6279b6
Patrick <patrick.schwindt14@gmail.com>
2019-01-25 10:26:07 -0700

    Prototypes

--- challenges/functions.js
+++ challenges/functions.js
@@ -46,15 +46,15 @@ function greeting(first, last){
 // Explanation: 
 
 
-// const external = "I'm outside the function";
+const external = "I'm outside the function";
 
-// function myFunction() {
-//   console.log(external);
-//   const internal = "Hello! I'm inside myFunction!";
+function myFunction() {
+  console.log(external);
+  const internal = "Hello! I'm inside myFunction!";
 
-//   function nestedFunction() {
-//     console.log(internal);
-//   };
-//   nestedFunction();
-// }
-// myFunction();
+  function nestedFunction() {
+    console.log(internal);
+  };
+  nestedFunction();
+}
+myFunction();
